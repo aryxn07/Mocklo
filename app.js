c9ddd328225ec5b66c2f9fe942ed55589eb6d24d
Aryan <rajaryan5614@gmail.com>
2026-08-28 09:48:37 +0530

    Fix visitor IP detection

--- app.js
+++ app.js
@@ -17,49 +17,54 @@ async function main() {
 }
 
 main()
-.then(() => {
+  .then(() => {
     console.log("Connected to MongoDB Atlas");
-})
-.catch(err => console.error(err));
+  })
+  .catch(err => console.error(err));
 
-app.set("view engine" ,"ejs");
-app.set("views" , path.join(__dirname, "views"));
-app.use(express.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-const ejsMate  = require("ejs-mate");
+const ejsMate = require("ejs-mate");
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname , "/public"))); 
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/locations", async(req,res)=>{
-    const request = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.GEO_API_KEY}`) // &ipAddress=83.44.92.17 (Spain)
-    const jsonResponse = await request.json()
-    let data={};
-    data.lat=jsonResponse.location.lat;
-    data.lng=jsonResponse.location.lng;
-    data.country=jsonResponse.location.country;
-    data.city=jsonResponse.location.city;
-    console.log(data.country);                                     
-    console.log(data);
+app.get("/locations", async (req, res) => {
+  
+  console.log("IP:", req.ip);
+  console.log("Forwarded:", req.headers["x-forwarded-for"]);
 
-//  IN -> India
+  const request = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.GEO_API_KEY}&ipAddress=${ip}`); // &ipAddress=83.44.92.17 (Spain)
+  const jsonResponse = await request.json()
+  let data = {};
+  data.lat = jsonResponse.location.lat;
+  data.lng = jsonResponse.location.lng;
+  data.country = jsonResponse.location.country;
+  data.city = jsonResponse.location.city;
+  console.log(data.country);
+  console.log(data);
 
-const countryMap = {
-  IN: "India",
-  US: "United States",
-  DE: "Germany",
-  FR: "France",
-  ES: "Spain",
-  CN: "China"
-};
-const response = await User.find({
-  country: countryMap[data.country]
-});
-    console.log(response);
-    res.render("locations/index.ejs",{data,response});
+  //  IN -> India
+
+  const countryMap = {
+    IN: "India",
+    US: "United States",
+    DE: "Germany",
+    FR: "France",
+    ES: "Spain",
+    CN: "China"
+  };
+  const response = await User.find({
+    country: countryMap[data.country]
+  });
+  console.log(response);
+  res.render("locations/index.ejs", { data, response });
 })
 
-app.listen(8080, ()=>{
-    console.log("Server is listening to port 8080");
+app.listen(8080, () => {
+  console.log("Server is listening to port 8080");
 })
